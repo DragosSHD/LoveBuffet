03d36cf37f4b8450dd2da895c902278beb6b9ba4
DragosSHD <dragos.dragos.502@gmail.com>
2022-04-04 18:14:10 +0300

    Implement navigation bar layout.

---
+++ front-end/lovebuffet-vue/src/components/Navbar.vue
@@ -0,0 +1,92 @@
+<template>
+  <nav>
+    <n-grid x-gap="12" cols="3 m:4 l:5" item-responsive responsive="screen">
+      <n-gi class="grid-item center" span="1 m:0">
+        <n-icon :size="iconSize" color="#23b35d">
+          <Bars/>
+        </n-icon>
+      </n-gi>
+      <n-gi class="grid-item center">
+        <n-image alt="love-buffet-logo"
+                 src="./src/assets/img/logo.png"
+                 width="120"
+                 preview-disabled></n-image>
+      </n-gi>
+      <n-gi class="grid-item" span="0 m:2 l:3">
+        <n-menu class="menu" mode="horizontal" :options="menuOptions"/>
+      </n-gi>
+      <n-gi class="grid-item center">
+        <n-icon :size="iconSize" color="#ea3c5d">
+          <UserRegular/>
+        </n-icon>
+      </n-gi>
+    </n-grid>
+    <n-grid x-gap="12" cols="3">
+
+    </n-grid>
+  </nav>
+</template>
+
+<script>
+import { NMenu, NGrid, NGi, NImage, NIcon } from "naive-ui";
+import { Bars, UserRegular } from "@vicons/fa";
+import { ref, h } from "vue";
+import { RouterLink } from "vue-router";
+
+const menuOptions = [
+  {
+    label: "Preferences",
+    key: "preferences",
+    disabled: true,
+  },
+  {
+    label: "Favourites",
+    key: "favourites",
+    disabled: true
+  },
+  {
+    label: "History",
+    key: "history",
+    disabled: true
+  }
+]
+
+export default {
+  name: 'Navbar',
+  components: {
+    NMenu, NGrid, NGi, NImage, Bars, NIcon,
+    UserRegular
+  },
+  data: () => {
+    return {
+      iconSize: 30
+    }
+  },
+  setup() {
+    return {
+      activeKey: ref(null),
+      menuOptions
+    };
+  }
+};
+</script>
+
+<style scoped>
+  nav{
+    -webkit-box-shadow: 0 5px 7px 0 rgba(0,0,0,0.37);
+    box-shadow: 0 5px 7px 0 rgba(0,0,0,0.37);
+    padding: 15px 0 15px 0;
+  }
+  .menu {
+    font-size: 1.5em;
+    font-weight: bold;
+    color: #23b35d;
+  }
+  .grid-item {
+    display: flex;
+    align-items: center;
+  }
+  .center {
+    justify-content: center;
+  }
+</style>
