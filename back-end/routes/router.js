c64d6baf53ed8f5bacaac4c2a97527ff296b5a14
DragosSHD <dragos.dragos.502@gmail.com>
2022-06-15 18:16:46 +0300

    Fix typo

--- back-end/routes/router.js
+++ back-end/routes/router.js
@@ -5,7 +5,7 @@ rootRouter.use('/users', require("./user.routes"));
 rootRouter.use('/address', require("./address.routes"));
 rootRouter.use('/auth', require("./authentication.routes"));
 rootRouter.use('/foodApi', require("./foodApi.routes"));
-rootRouter.use('/recipe', require("./recipe.routes"));
+rootRouter.use('/recipes', require("./recipe.routes"));
 rootRouter.use('/history', require("./history.routes"));
 rootRouter.use('/favourites', require("./favourites.routes"));
 
